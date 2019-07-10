d1a2e8677fe84c719e0077a40fca6ef053039959
Michael Williams <manicdesigner@gmail.com>
2019-07-10 18:39:58 -0400

    feat(reducer/index) + Increment case

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import { INCREMENT, DECREMENT } from '../actions';
+import { breakStatement } from '@babel/types';
 
 const initialState = {
   count: 0
-}
+};
 
 // Our reducer that handles our two action cases:
 // increment and decrement. It receives the state
@@ -12,7 +13,12 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+      const { count } = action.payload;
+      // Fill in the body of this case
+      const increaseCount = Number(count) + state[count];
+      return {
+        count: increaseCount
+      };
     case DECREMENT:
     // Fill in the body of this case
     default:
